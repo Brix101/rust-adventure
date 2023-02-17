819063c03bf12faef2626d21b035592fbee97cdf
Brixter Porras <brixterporras@gmail.com>
2023-02-17 14:48:25 +0800

    feat: added type validation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,10 +15,13 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-
+        
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
         };
 
         println!("You guessed: {guess}");
