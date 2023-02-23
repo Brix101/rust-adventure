abbbd7b675574d5e564e46fd09c9120d2be4bed2
Brixter Porras <brixterporras@gmail.com>
2023-02-23 15:32:31 +0800

    feat: added design

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,12 +4,10 @@ use std::io;
 
 fn main() {
     loop {
-        let limit: u32 = 100;
-        let secret_number = rand::thread_rng().gen_range(1..=limit);
-        let hint_number = rand::thread_rng().gen_range(1..=10);
-        let nearest_number = (secret_number / 10) * hint_number;
-        let mut guessed_time: u32 = 10;
+        let (limit, secret_number, hint_number, nearest_number) = secret_generator();
+        let mut guessed_time: u32 = 5;
 
+        println!("<><><><><><><><><><><><><><><><><><><><><><><><><><>");
         println!("Guess the number!");
         println!("Hint!: {}, {}", nearest_number, hint_number);
 
@@ -18,13 +16,15 @@ fn main() {
 
             match guessed_time {
                 0 => {
-                    println!("Game Over!!!");
                     println!("Correct number: {}", secret_number);
-                    println!("-------------------------------------------------------------");
+                    println!("!!!!!!!!!!!!!!!!!!!");
+                    println!("!!!! Game Over !!!!");
+                    println!("!!!!!!!!!!!!!!!!!!!");
                     break;
                 }
                 _ => {
-                    println!("Remaining guess {}", guessed_time);
+                    println!("");
+                    println!("<<<<<<<<<<000--- Remaining guess {guessed_time} ---000>>>>>>>>>>");
                     println!("Please input your guess: ");
                     io::stdin()
                         .read_line(&mut guess)
@@ -35,7 +35,10 @@ fn main() {
             let guess: u32 = match guess.trim().parse() {
                 Ok(num) => {
                     if num > limit {
-                        println!("Please type a number lower than 100");
+                        println!("Please type a number lower than {limit}");
+                        continue;
+                    } else if num <= 0 {
+                        println!("Please type a number greater than 0");
                         continue;
                     } else {
                         guessed_time -= 1;
@@ -58,13 +61,22 @@ fn main() {
                     println!("Too big!");
                 }
                 Ordering::Equal => {
-                    println!("You win!");
-                    for _i in 1..10 {
-                        print!("xo")
-                    }
+                    println!("===================");
+                    println!("===== You win =====");
+                    println!("===================");
                     break;
                 }
             }
         }
     }
 }
+
+fn secret_generator() -> (u32, u32, u32, u32) {
+    let limit: u32 = 100;
+    let secret_number = rand::thread_rng().gen_range(1..=limit);
+    let hint_number = rand::thread_rng().gen_range(1..=10);
+    let nearest_number = (secret_number / 10) * hint_number;
+
+    return (limit, secret_number, hint_number, nearest_number);
+}
+
