ecb234536c2951bd85225ffe1cd23303af9c8779
Brixter Porras <brixterporras@gmail.com>
2023-02-23 20:38:53 +0800

    feat: added difficulty level

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,67 +4,95 @@ use std::io;
 
 fn main() {
     loop {
-        let (limit, secret_number, hint_number, nearest_number) = secret_generator();
-        let mut guessed_time: u32 = 5;
+        let mut diff_input = String::new();
 
-        println!("<><><><><><><><><><><><><><><><><><><><><><><><><><>");
-        println!("Guess the number!");
-        println!("Hint!: {}, {}", nearest_number, hint_number);
+        println!("<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>");
+        println!("<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>");
+        println!("<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>");
+        println!("Select Difficulty: [1]easy | [2]medium | [3]hard | [0]quit ");
 
-        loop {
-            let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut diff_input)
+            .expect("Failed to read line");
 
-            match guessed_time {
-                0 => {
-                    println!("Correct number: {}", secret_number);
-                    println!("!!!!!!!!!!!!!!!!!!!");
-                    println!("!!!! Game Over !!!!");
-                    println!("!!!!!!!!!!!!!!!!!!!");
-                    break;
-                }
-                _ => {
-                    println!("");
-                    println!("<<<<<<<<<<000--- Remaining guess {guessed_time} ---000>>>>>>>>>>");
-                    println!("Please input your guess: ");
-                    io::stdin()
-                        .read_line(&mut guess)
-                        .expect("Failed to read line");
-                }
+        let mut diffculty: u32 = match diff_input.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("{e}, Please type a number!");
+                continue;
             }
+        };
 
-            let guess: u32 = match guess.trim().parse() {
-                Ok(num) => {
-                    if num > limit {
-                        println!("Please type a number lower than {limit}");
-                        continue;
-                    } else if num <= 0 {
-                        println!("Please type a number greater than 0");
-                        continue;
-                    } else {
-                        guessed_time -= 1;
-                        num
+        match diffculty {
+            1 => diffculty = 10,
+            2 => diffculty = 5,
+            3 => diffculty = 3,
+            0 => break,
+            _ => continue,
+        }
+
+        if diffculty > 0 {
+            let (limit, secret_number, hint_number, nearest_number) = secret_generator();
+
+            println!("<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>0<>");
+            println!("Guess the number!");
+            println!("Hint!: {}, {}", nearest_number, hint_number);
+
+            loop {
+                let mut guess = String::new();
+
+                match diffculty {
+                    0 => {
+                        println!("Correct number: {}", secret_number);
+                        println!("===================");
+                        println!("==== Game Over ====");
+                        println!("===================");
+                        break;
+                    }
+                    _ => {
+                        println!("");
+                        println!("<<<<<<<<<<000--- Remaining guess {diffculty} ---000>>>>>>>>>>");
+                        println!("Please input your guess: ");
+                        io::stdin()
+                            .read_line(&mut guess)
+                            .expect("Failed to read line");
                     }
                 }
-                Err(e) => {
-                    println!("{e}, Please type a number!");
-                    continue;
-                }
-            };
 
-            println!("You guessed: {guess}");
+                let guess: u32 = match guess.trim().parse() {
+                    Ok(num) => {
+                        if num > limit {
+                            println!("Please type a number lower than {limit}");
+                            continue;
+                        } else if num <= 0 {
+                            println!("Please type a number greater than 0");
+                            continue;
+                        } else {
+                            diffculty -= 1;
+                            num
+                        }
+                    }
+                    Err(e) => {
+                        println!("{e}, Please type a number!");
+                        continue;
+                    }
+                };
 
-            match guess.cmp(&secret_number) {
-                Ordering::Less => {
-                    println!("Too small!");
-                }
-                Ordering::Greater => {
-                    println!("Too big!");
-                }
-                Ordering::Equal => {
-                    println!("===================");
-                    println!("===== You win =====");
-                    println!("===================");
-                    break;
+                println!("You guessed: {guess}");
+
+                match guess.cmp(&secret_number) {
+                    Ordering::Less => {
+                        println!("Too small!");
+                    }
+                    Ordering::Greater => {
+                        println!("Too big!");
+                    }
+                    Ordering::Equal => {
+                        println!("===================");
+                        println!("===== You win =====");
+                        println!("===================");
+                        break;
+                    }
                 }
             }
         }
@@ -79,4 +107,3 @@ fn secret_generator() -> (u32, u32, u32, u32) {
 
     return (limit, secret_number, hint_number, nearest_number);
 }
-
