96e554d423b025b5ea3e965e04da401662ba2087
Brixter Porras <brixterporras@gmail.com>
2023-02-27 14:44:42 +0800

    feat: added aoc day 01

--- aoc-2022-01/target/debug/deps/aoc_2022_01-cd66aacae65c37b5.d
+++
@@ -1,5 +0,0 @@
-/home/brix/Repos/rust-journey/aoc-2022-01/target/debug/deps/aoc_2022_01-cd66aacae65c37b5.rmeta: src/main.rs
-
-/home/brix/Repos/rust-journey/aoc-2022-01/target/debug/deps/aoc_2022_01-cd66aacae65c37b5.d: src/main.rs
-
-src/main.rs:
