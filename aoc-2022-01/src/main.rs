43d4fa950f0e290b64e7e10e900fa6e58fc9abbc
Brixter Porras <brixterporras@gmail.com>
2023-02-27 15:04:25 +0800

    feat: finnished aoc day 01

--- aoc-2022-01/src/main.rs
+++ aoc-2022-01/src/main.rs
@@ -1,7 +1,19 @@
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("./input.txt").expect("Cannot load file");
+    let content = fs::read_to_string("input.txt").expect("Cannot load file");
 
-    println!("Hello, world! {file}");
+    let counts = content
+        .split("\n\n")
+        .map(|chunk| -> usize { chunk.split("\n").map(|row| row.parse().unwrap_or(0)).sum() });
+
+    let mut v = counts.collect::<Vec<_>>();
+    v.sort();
+
+    let last_idx = v.len() - 1;
+
+    for x in &v {
+        println!("{}", x);
+    }
+    println!("The highest count is {}", v[last_idx]);
 }
